c67d366560d51c21a5b6b90eb71d203f91136f4d
ReflexAngle <108433343+ReflexAngle@users.noreply.github.com>
2025-02-19 21:10:29 -0600

    player moves
    the player can now move along the x and y axis

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,12 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     private bool canMove = true;
     private bool canAttack = true;
+    private bool canJump = true;
+    [SerializeField] private int lockVeritcalMove;
+    [SerializeField] private float speed = 5f;
+    private float gravity = 9.81f;
+    private Vector2 moveDirection = Vector2.zero;
     private CharacterController controller;
+    public InputControlls playerControlls;
+    private InputAction move;
+
 
     public bool CanMove{
         get{return canMove;}
@@ -17,15 +26,27 @@ public class PlayerController : MonoBehaviour
         get{return canAttack;}
         set{this.canAttack = value;}
     }
+    public bool CanJump{
+        get{return canJump;}
+        set{this.canJump = value;}
+    }
+    public int LockVeritcalMove{
+        get{return lockVeritcalMove;}
+        set{this.lockVeritcalMove = value;}
+    }
 
     void OnEnable(){
-        
+        move = playerControlls.Movement.WASD;
+
+        move.Enable();
     }
     void OnDisable(){
+        move.Disable();
 
     }
     void Awake(){
         controller = GetComponent<CharacterController>();
+        playerControlls = new InputControlls();
 
     }
     // Start is called before the first frame update
@@ -45,12 +66,25 @@ public class PlayerController : MonoBehaviour
         
     }
     private void BasicMovement(){
+        moveDirection = move.ReadValue<Vector2>();    
+
+        Vector3 _move = new Vector3(moveDirection.x, moveDirection.y, 0);
+        controller.Move(_move * speed * Time.deltaTime);
+
+    }
+    private void LockVerticalMove(){
 
     }
     private void Jumping(){
+        if(CanJump){
+
+        }
 
     }
     private void Attacking(){
+        if(CanAttack){
+
+        }
 
     }
 }
